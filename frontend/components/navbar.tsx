1c52285658dff8963a2d8ee0d72ee52d549ad749
lethe2211 <lethe2211@gmail.com>
2023-02-19 13:39:59 +0900

    Install tailwind css

---
+++ frontend/components/navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+const NavBar: React.FC = () => {
+  return (
+    <nav className="flex items-center justify-between flex-wrap bg-[#E4021C] p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <Link href="/">
+          <span className="font-semibold text-xl tracking-tight">
+            Pokedex App
+          </span>
+        </Link>
+      </div>
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="text-sm lg:flex-grow">
+          <a
+            href="https://github.com/lethe2211/nginx-nextjs-nestjs-mysql-sample"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
+          >
+            Github
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
